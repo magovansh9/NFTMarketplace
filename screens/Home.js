bbcf432bb4f101e6d5cfbd9dc39349ded52050ff
Vansh Mago <vanshmago9@gmail.com>
2022-04-01 21:49:26 -0400

    Fix Pending: App Navigation

---
+++ screens/Home.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { View, SafeAreaView, FlatList } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+
+const Home = ({ navigation }) => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length) {
+      setNftData(filteredData);
+    } else {
+      setNftData(nftData);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.secondary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => (
+              <NFTCard data={item} navigation={navigation} />
+            )}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View
+            style={{ flex: 1, height: 300, backgroundColor: COLORS.white }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
